427bec3d784c15c89a1fce1d67d6fa8cffbeb823
ajankowi <252919@student.pwr.edu.pl>
2026-01-09 21:10:45 +0100

    Add Cmake

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+int multiplication(int a, int b)
+{
+    return a*b;
+}
+
+int main()
+{
+
+    int var_1 = 10;
+    int var_2 = 2;
+    
+
+    std::cout << "I am alive!: " << multiplication(var_1,var_2) << std::endl; 
+
+}
